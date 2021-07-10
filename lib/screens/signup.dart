34cd3bee8174ce698cd055f0dfbd89148dc7dd1e
Shradha1824 <shradhabj1824@gmail.com>
2021-07-10 10:22:43 -1200

    MODIFIED: Added ElevatedButton

--- lib/screens/signup.dart
+++ lib/screens/signup.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/screens/base_screen.dart';
 import 'package:hexcolor/hexcolor.dart';
@@ -11,143 +12,164 @@ class SignupState extends BaseScreenState {
     return AppBar(
       backgroundColor: HexColor('#446DFF'),
       title: Text(
-        'Welcome to fundonote application',
+        'Register to Fundoonote application',
         style: TextStyle(fontSize: 22.0, color: HexColor('#FFFFFF')),
       ),
       centerTitle: true,
     );
   }
 
-  var _userInput;
+  var _uInput;
 
   Widget getBody(BuildContext context) {
     return Center(
         child: Container(
             child: SizedBox(
-                child: ListView(children: <Widget>[
-      Container(
-          child: Column(children: <Widget>[
-        Container(
-            margin: EdgeInsets.all(20.0),
-            width: 500.0,
-            height: 70.0,
-            padding: EdgeInsets.only(left: 16, right: 16, top: 11),
-            child: TextField(
-                style: TextStyle(fontSize: 15.0, color: Colors.black),
-                textAlign: TextAlign.start,
-                decoration: InputDecoration(
-                    border: InputBorder.none,
-                    hintText: "First Name",
-                    hintStyle: TextStyle(fontSize: 15.0, color: Colors.black)),
-                keyboardType: TextInputType.text,
-                onChanged: (text) {
-                  var input = double.tryParse(text);
-                  if (input != null)
-                    setState(() {
-                      _userInput = input;
-                    });
-                }),
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.grey, width: 1),
-              borderRadius: BorderRadius.horizontal(),
-            )),
-        Container(
-            margin: EdgeInsets.all(20.0),
-            width: 500.0,
-            height: 70.0,
-            padding: EdgeInsets.only(left: 16, right: 16, top: 11),
-            child: TextField(
-                style: TextStyle(fontSize: 15.0, color: Colors.black),
-                textAlign: TextAlign.start,
-                decoration: InputDecoration(
-                    border: InputBorder.none,
-                    hintText: "Last Name",
-                    hintStyle: TextStyle(fontSize: 15.0, color: Colors.black)),
-                keyboardType: TextInputType.text,
-                onChanged: (text) {
-                  var input = double.tryParse(text);
-                  if (input != null)
-                    setState(() {
-                      _userInput = input;
-                    });
-                }),
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.grey, width: 1),
-              borderRadius: BorderRadius.horizontal(),
-            )),
-        Container(
-            margin: EdgeInsets.all(20.0),
-            width: 500.0,
-            height: 70.0,
-            padding: EdgeInsets.only(left: 16, right: 16, top: 11),
-            child: TextField(
-                style: TextStyle(fontSize: 15.0, color: Colors.black),
-                textAlign: TextAlign.start,
-                decoration: InputDecoration(
-                    border: InputBorder.none,
-                    hintText: "EmailId",
-                    hintStyle: TextStyle(fontSize: 15.0, color: Colors.black)),
-                keyboardType: TextInputType.text,
-                onChanged: (text) {
-                  var input = double.tryParse(text);
-                  if (input != null)
-                    setState(() {
-                      _userInput = input;
-                    });
-                }),
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.grey, width: 1),
-              borderRadius: BorderRadius.horizontal(),
-            )),
-        Container(
-            margin: EdgeInsets.all(20.0),
-            width: 500.0,
-            height: 70.0,
-            padding: EdgeInsets.only(left: 16, right: 16, top: 11),
-            child: TextField(
-                style: TextStyle(fontSize: 15.0, color: Colors.black),
-                textAlign: TextAlign.start,
-                decoration: InputDecoration(
-                    border: InputBorder.none,
-                    hintText: "Password",
-                    hintStyle: TextStyle(fontSize: 15.0, color: Colors.black)),
-                keyboardType: TextInputType.text,
-                onChanged: (text) {
-                  var input = double.tryParse(text);
-                  if (input != null)
-                    setState(() {
-                      _userInput = input;
-                    });
-                }),
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.grey, width: 1),
-              borderRadius: BorderRadius.horizontal(),
-            )),
-        Container(
-            margin: EdgeInsets.all(20.0),
-            width: 500.0,
-            height: 70.0,
-            padding: EdgeInsets.only(left: 16, right: 16, top: 11),
-            child: TextField(
-                style: TextStyle(fontSize: 15.0, color: Colors.black),
-                textAlign: TextAlign.start,
-                decoration: InputDecoration(
-                    border: InputBorder.none,
-                    hintText: "Confirm Password",
-                    hintStyle: TextStyle(fontSize: 15.0, color: Colors.black)),
-                keyboardType: TextInputType.text,
-                onChanged: (text) {
-                  var input = double.tryParse(text);
-                  if (input != null)
-                    setState(() {
-                      _userInput = input;
-                    });
-                }),
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.grey, width: 1),
-              borderRadius: BorderRadius.horizontal(),
-            ))
-      ]))
-    ]))));
+      child: ListView(
+        children: <Widget>[
+          Container(
+              margin: EdgeInsets.all(15),
+              child: Column(children: <Widget>[
+                Container(
+                    margin: EdgeInsets.all(20.0),
+                    width: 500.0,
+                    height: 70.0,
+                    padding: EdgeInsets.only(left: 16, right: 16, top: 11),
+                    child: TextField(
+                        style: TextStyle(fontSize: 15.0, color: Colors.black),
+                        textAlign: TextAlign.start,
+                        decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: "First Name",
+                            hintStyle:
+                                TextStyle(fontSize: 15.0, color: Colors.black)),
+                        keyboardType: TextInputType.text,
+                        onChanged: (text) {
+                          var input = double.tryParse(text);
+                          if (input != null)
+                            setState(() {
+                              _uInput = input;
+                            });
+                        }),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey, width: 1),
+                      borderRadius: BorderRadius.horizontal(),
+                    )),
+                Container(
+                    margin: EdgeInsets.all(20.0),
+                    width: 500.0,
+                    height: 70.0,
+                    padding: EdgeInsets.only(left: 16, right: 16, top: 11),
+                    child: TextField(
+                        style: TextStyle(fontSize: 15.0, color: Colors.black),
+                        textAlign: TextAlign.start,
+                        decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: "Last Name",
+                            hintStyle:
+                                TextStyle(fontSize: 15.0, color: Colors.black)),
+                        keyboardType: TextInputType.text,
+                        onChanged: (text) {
+                          var input = double.tryParse(text);
+                          if (input != null)
+                            setState(() {
+                              _uInput = input;
+                            });
+                        }),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey, width: 1),
+                      borderRadius: BorderRadius.horizontal(),
+                    )),
+                Container(
+                    margin: EdgeInsets.all(20.0),
+                    width: 500.0,
+                    height: 70.0,
+                    padding: EdgeInsets.only(left: 16, right: 16, top: 11),
+                    child: TextField(
+                        style: TextStyle(fontSize: 15.0, color: Colors.black),
+                        textAlign: TextAlign.start,
+                        decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: "Email Id",
+                            hintStyle:
+                                TextStyle(fontSize: 15.0, color: Colors.black)),
+                        keyboardType: TextInputType.text,
+                        onChanged: (text) {
+                          var input = double.tryParse(text);
+                          if (input != null)
+                            setState(() {
+                              _uInput = input;
+                            });
+                        }),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey, width: 1),
+                      borderRadius: BorderRadius.horizontal(),
+                    )),
+                Container(
+                    margin: EdgeInsets.all(20.0),
+                    width: 500.0,
+                    height: 70.0,
+                    padding: EdgeInsets.only(left: 16, right: 16, top: 11),
+                    child: TextField(
+                        style: TextStyle(fontSize: 15.0, color: Colors.black),
+                        textAlign: TextAlign.start,
+                        decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: "Password",
+                            hintStyle:
+                                TextStyle(fontSize: 15.0, color: Colors.black)),
+                        keyboardType: TextInputType.text,
+                        onChanged: (text) {
+                          var input = double.tryParse(text);
+                          if (input != null)
+                            setState(() {
+                              _uInput = input;
+                            });
+                        }),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey, width: 1),
+                      borderRadius: BorderRadius.horizontal(),
+                    )),
+                Container(
+                    margin: EdgeInsets.all(20.0),
+                    width: 500.0,
+                    height: 70.0,
+                    padding: EdgeInsets.only(left: 16, right: 16, top: 11),
+                    child: TextField(
+                        style: TextStyle(fontSize: 15.0, color: Colors.black),
+                        textAlign: TextAlign.start,
+                        decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: "Confirm Password",
+                            hintStyle:
+                                TextStyle(fontSize: 15.0, color: Colors.black)),
+                        keyboardType: TextInputType.text,
+                        onChanged: (text) {
+                          var input = double.tryParse(text);
+                          if (input != null)
+                            setState(() {
+                              _uInput = input;
+                            });
+                        }),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey, width: 1),
+                      borderRadius: BorderRadius.horizontal(),
+                    )),
+              ])),
+          Column(children: [
+            ElevatedButton(
+              onPressed: () {},
+              child: Text('Submit'),
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Colors.blue),
+                padding: MaterialStateProperty.all(
+                    EdgeInsets.only(top: 20, left: 65, right: 65, bottom: 20)),
+                textStyle: MaterialStateProperty.all(TextStyle(fontSize: 25)),
+              ),
+            )
+          ]),
+        ],
+      ),
+    )));
   }
 }
