344e5f241b991ba3c39a026588e1b82428ce0993
Shradha1824 <83284807+Shradha1824@users.noreply.github.com>
2021-07-24 20:12:45 -1200

    Delete firebase_config.dart

--- lib/constance/api_constance/firebase_config.dart
+++
@@ -1,6 +0,0 @@
-class FireBase {
-  static String appId = '1:1049358119550:android:57ec7d4ec73162540ade2f';
-  static String apiKey = 'AIzaSyBFWtaf3irT3sZXxRTQNb0mQ7bl2lZ5hE4';
-
-  static String ProjectId = 'fundoonotes-79897';
-}
